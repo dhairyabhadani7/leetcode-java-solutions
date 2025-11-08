7b510cdcc9ce87a1313d7c10e271ea084aa353d7
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-11-08 15:22:19 +0530

    Updated optimized 2 sum problem

--- src/main/java/com/dhairya/leetcode/easy/TwoSum.java
+++ src/main/java/com/dhairya/leetcode/easy/TwoSum.java
@@ -1,15 +1,21 @@
 package com.dhairya.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complecent = target-nums[i];
+            if(hm.containsKey(complecent)){
+                return new int[]{hm.get(complecent),i};
             }
+            hm.put(nums[i],i);
+
         }
         return new int[]{};
+
     }
 }
 
