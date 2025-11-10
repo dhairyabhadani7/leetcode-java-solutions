4a914eac3532707859e02c095a35d199f5f88ee6
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-11-10 22:07:05 +0530

    Added isPalindrome solution

---
+++ src/main/java/com/dhairya/leetcode/easy/isPalindrome.java
@@ -0,0 +1,24 @@
+package com.dhairya.leetcode.easy;
+
+class PalindromeCheck{
+    public static boolean isPalindrome(int num){
+
+        if(num<0) return false;
+        int x=num;
+        int reverse=0;
+        while(num>0){
+            int rem= num%10;
+            reverse=reverse*10+rem;
+            num=num/10;
+        }
+        return reverse==x;
+    }
+}
+public class isPalindrome {
+    public static void main(String[] args) {
+        boolean res=PalindromeCheck.isPalindrome(-121);
+        System.out.println(res);
+    }
+    }
+
+
