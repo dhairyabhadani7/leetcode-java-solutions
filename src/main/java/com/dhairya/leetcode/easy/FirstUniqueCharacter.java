87463cc667c32c7a8036bfe7b98679ff32b41301
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-11-15 23:58:09 +0530

    Added First Unique Character solution

---
+++ src/main/java/com/dhairya/leetcode/easy/FirstUniqueCharacter.java
@@ -0,0 +1,27 @@
+package com.dhairya.leetcode.easy;
+
+class Sol12 {
+    public static int firstUniqChar(String s) {
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
+
+
+public class FirstUniqueCharacter {
+    public static void main(String[] args) {
+     String s = "leetcode";
+     int res= Sol12.firstUniqChar(s);
+     System.out.println(res);
+    }
+}
