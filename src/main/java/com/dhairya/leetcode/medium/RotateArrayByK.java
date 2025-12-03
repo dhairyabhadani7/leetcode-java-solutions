42129f2b8e2d4707518f891fdcb94db0b5c3c2a7
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-12-03 12:34:07 +0530

    Added common RotateArrayByK Solution

--- src/main/java/com/dhairya/leetcode/medium/RotateArrayByK.java
+++ src/main/java/com/dhairya/leetcode/medium/RotateArrayByK.java
@@ -2,9 +2,9 @@ package com.dhairya.leetcode.medium;
 
 import java.util.Arrays;
 
-class RotateArray{
+class Rotate{
     private void reverse(int[] nums, int left, int right){
-        while (left < right) {
+        while(left<right){
             int temp=nums[left];
             nums[left]=nums[right];
             nums[right]=temp;
@@ -12,22 +12,35 @@ class RotateArray{
             right--;
         }
     }
-    public void rotate(int[] nums, int k) {
-        k=k% nums.length;
-        reverse(nums,0, nums.length-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k, nums.length-1);
+    public int[] leftRotateByK(int[] nums, int k){
+        if(nums.length==0) return nums;
+        k=k%nums.length;
+        if(k==0) return nums;
 
-        System.out.println(Arrays.toString(nums));
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+        return nums;
+    }
+
+    public int[] rightRotateByK(int[] nums, int k){
+        if(nums.length==0) return nums;
+        k=k%nums.length;
+        if(k==0) return nums;
+
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+        return nums;
     }
 }
 
 public class RotateArrayByK {
     public static void main(String[] args) {
-       int[] arr={1,2,3,4,5,6,7};
-       RotateArray rot=new RotateArray();
-       rot.rotate(arr,3);
-
+        int[] arr={1,2,3,4,5,6,7};
+        Rotate rotate = new Rotate();
+        int[] res = rotate.rightRotateByK(arr,3);
+        System.out.println(Arrays.toString(res));
 
     }
 }
